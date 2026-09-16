2933960c1183d9834bf5e8b43ae3125fa4c965cd
Miroslaw Drzazga <mdrzazga33@gmail.com>
2026-09-16 14:14:59 +0200

    AL-287 LICENSE,  NOTICE do porzeglarki

---
+++ przegladarka/gui/app/NOTICE/route.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2026 Algor Informatyzcja Przedsiębiorstw Sp. z o.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://gnu.org>.
+ */
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  try {
+    const filePath = path.join(process.cwd(), 'NOTICE.txt');
+    const noticeContent = fs.readFileSync(filePath, 'utf8');
+
+    return new NextResponse(noticeContent, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        'Cache-Control': 'public, max-age=86400, immutable',
+      },
+    });
+  } catch (error) {
+    return new NextResponse('Internal Server Error: NOTICE file not found', { status: 500 });
+  }
+}
